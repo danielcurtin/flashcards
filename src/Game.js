f9f1960373f6e5d47a728c8699c905a39090bee6
Daniel Curtin <dcurtin.work@gmail.com>
2023-01-30 19:44:35 -0700

    Build out game functionality

--- src/Game.js
+++ src/Game.js
@@ -2,17 +2,35 @@ const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
-class Game {
-  constructor() {}
+const Card = require('./Card');
+const Deck = require('./Deck');
+const Round = require('./Round');
+const { prototype } = require('mocha');
 
-  printMessage(deck, round) {
+class Game {
+  constructor(currentRound) {
+    this.currentRound = currentRound;
+  };
+
+  printMessage(deck) {
     console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
 -----------------------------------------------------------------------`)
-  }
+  };
 
   printQuestion(round) {
       util.main(round);
-  }
-}
+  };
+
+  start() {
+    const newCards = data.prototypeData.map((element) => {
+      return new Card(element.id, element.question, element.answers, element.correctAnswer);
+    });
+    const newDeck = new Deck(newCards);
+    const newRound = new Round(newDeck);
+
+    this.printMessage(newRound.deck);
+    this.printQuestion();
+  };
+};
 
 module.exports = Game;
